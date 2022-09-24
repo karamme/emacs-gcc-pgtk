f8504a321e9ccde96ddfd437c1d17f6c9909616f
karamme <kaischramme@gmail.com>
2022-09-24 22:52:05 +0200

    Revert changes

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
-docker build -t karamme/emacs-gcc-pgtk:latest .
-id=$(docker create karamme/emacs-gcc-pgtk)
+docker build -t konstare/emacs-gcc-pgtk:latest .
+id=$(docker create konstare/emacs-gcc-pgtk)
 docker cp $id:/opt/deploy .
